c1176c0900d4a2ac99ed68645b4fa33d0b9578c6
Adrian Marin <adrian@adrianthedev.com>
2025-02-21 16:30:27 +0200

    Bump version for 0.2.2

--- app/assets/javascripts/marksmith_controller-no-stimulus.esm.js
+++ app/assets/javascripts/marksmith_controller-no-stimulus.esm.js
@@ -1,5 +1,5 @@
 /*!
-Marksmith 0.2.1
+Marksmith 0.2.2
 */
 var MarksmithController = (function (stimulus) {
     'use strict';
--- dist/marksmith-core.esm.js
+++ dist/marksmith-core.esm.js
@@ -1,5 +1,5 @@
 /*!
-Marksmith 0.2.1
+Marksmith 0.2.2
 */
 import '@github/markdown-toolbar-element';
 import { Controller } from '@hotwired/stimulus';
