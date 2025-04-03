db4e67858aab0c3a62ceb2d8b8fcf59b4e59cce2
Adrian Marin <adrian@adrianthedev.com>
2025-04-03 16:02:18 +0300

    wip

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
 import { defineConfig } from 'vite'
-import RubyPlugin from 'vite-plugin-ruby'
+import Rails from 'vite-plugin-rails'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
   plugins: [
-    RubyPlugin(),
-    tailwindcss()
+    tailwindcss(),
+    Rails({
+      envVars: { RAILS_ENV: 'development' },
+    }),
   ],
 })
