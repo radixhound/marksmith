a4e0d078f79466fea7f7b080219cb5a09c981b74
Paul Bob <paul.ionut.bob@gmail.com>
2025-04-04 10:56:11 +0300

    enhancement: field level option to disable gallery

--- lib/marksmith/fields/markdown_field.rb
+++ lib/marksmith/fields/markdown_field.rb
@@ -2,6 +2,8 @@ module Marksmith
   module Fields
     class MarkdownField < Avo::Fields::BaseField
       def initialize(id, **args, &block)
+        @media_library = args[:media_library].nil? ? true : args[:media_library]
+
         super(id, **args, &block)
 
         hide_on :index
@@ -9,6 +11,10 @@ module Marksmith
 
       def view_component_namespace
         "Marksmith::MarkdownField"
+      end
+
+      def gallery_enabled?
+        Avo::MediaLibrary.configuration.enabled && @media_library
       end
     end
   end
